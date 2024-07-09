4c43e417dc7d1f1ca2a4a129da5524c90cb21c6c
Grzegorz <grzegorz_sroda@wp.pl>
2024-07-09 18:40:54 +0200

    Index.html fragments refactoring for better readibility.

--- src/main/java/pl/grzegorz/snapshotstories/controller/FormController.java
+++ src/main/java/pl/grzegorz/snapshotstories/controller/FormController.java
@@ -1,8 +1,8 @@
 package pl.grzegorz.snapshotstories.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,20 +13,14 @@ import pl.grzegorz.snapshotstories.service.FormService;
 @RequestMapping("/submitForm")
 public class FormController {
 
-
     @Autowired
     private FormService formService;
 
     @PostMapping
-    public String submitForm(@RequestParam String name,
-                             @RequestParam String email,
-                             @RequestParam String phone,
-                             @RequestParam String message) {
-
-        System.out.println("Name: " + name);
-        System.out.println("Email: " + email);
-        System.out.println("Phone: " + phone);
-        System.out.println("Message: " + message);
+    public ResponseEntity<String> submitForm(@RequestParam String name,
+                                             @RequestParam String email,
+                                             @RequestParam String phone,
+                                             @RequestParam String message) {
 
         FormData formData = new FormData();
         formData.setName(name);
@@ -37,8 +31,6 @@ public class FormController {
         formService.saveFormData(formData);
 
 
-        return "redirect:/success";
+        return ResponseEntity.ok("Form submitted successfully");
     }
-
-
 }
